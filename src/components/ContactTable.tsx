5ddae646843e3493409a9e7d45f1621e7e36964c
Tien Nguyen Khac <tiennguyenkhac1702@gmail.com>
2020-03-11 18:08:27 +1300

    Implement basic sort

--- src/components/ContactTable.tsx
+++ src/components/ContactTable.tsx
@@ -3,26 +3,61 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import React, { useCallback, useState } from "react";
 import { Table } from "reactstrap";
 
-const ContactTable = ({ data }: any) => {
-  const Rows = useCallback(
-    () =>
-      data.map(({ _id, name, email, phone }: any) => (
-        <tr key={_id}>
-          <td>{name}</td>
-          <td>{email}</td>
-          <td>{phone}</td>
-        </tr>
-      )),
-    []
+export type Contact = {
+  _id: string;
+  name: string;
+  email: string;
+  phone: string;
+};
+
+export type ContactTableProps = {
+  data: Contact[];
+};
+
+const ContactTable = ({ data }: ContactTableProps) => {
+  const [sortKey, setSortKey] = useState<keyof Contact | undefined>();
+  const [sortOrder, setSortOrder] = useState<"asc" | "desc">("desc");
+
+  const onRowClick = useCallback(
+    (key: keyof Contact) => () => {
+      setSortKey(key);
+      setSortOrder(order =>
+        order === "desc" || key !== sortKey ? "asc" : "desc"
+      );
+    },
+    [sortKey]
   );
+
+  const Rows = useCallback(() => {
+    const sortedData =
+      sortKey === undefined
+        ? data
+        : data.sort((a, b) =>
+            sortOrder === "asc"
+              ? a[sortKey].localeCompare(b[sortKey])
+              : b[sortKey].localeCompare(a[sortKey])
+          );
+
+    return (
+      <>
+        {sortedData.map(({ _id, name, email, phone }) => (
+          <tr key={_id}>
+            <td>{name}</td>
+            <td>{email}</td>
+            <td>{phone}</td>
+          </tr>
+        ))}
+      </>
+    );
+  }, [data, sortKey, sortOrder]);
 
   return (
     <Table className="App-table">
       <thead>
         <tr>
-          <th>Name</th>
-          <th>Email</th>
-          <th>Contact Number</th>
+          <th onClick={onRowClick("name")}>Name</th>
+          <th onClick={onRowClick("email")}>Email</th>
+          <th onClick={onRowClick("phone")}>Contact Number</th>
         </tr>
       </thead>
       <tbody>
