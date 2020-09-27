b73ff88b978bc57a19968f832266457161e7ec46
Brian Lee <LSH245@hotmail.com>
2020-09-27 16:19:44 +1300

    Add user reducer

---
+++ src/ducks/user/actions.ts
@@ -0,0 +1,5 @@
+import { createAction } from "redux-act";
+
+const setLoadingStatus = createAction("USER/SET_LOADING_STATUS");
+
+export { setLoadingStatus };
--- src/index.js
+++ src/index.js
@@ -1,10 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware, combineReducers } from "redux";
+import thunkMiddleware from "redux-thunk";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// TODO: need to import typescript and resolve this properly.
+import reducers from "./ducks/index.ts";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+
+import "./index.css";
+
+const createAppStore = (getDependencies, reducers) => {
+  const createStoreWithMiddleware = applyMiddleware(
+    thunkMiddleware.withExtraArgument(getDependencies)
+  )(createStore);
+  return createStoreWithMiddleware(
+    combineReducers(reducers),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  );
+};
+
+const store = createAppStore(() => {}, reducers);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
